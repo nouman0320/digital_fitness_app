720d4467d00dada69c0c98e3d51f44da76bca508
Nouman Arshad <noumanarshad0320@gmail.com>
2019-11-07 17:00:49 +0500

    Added components and routes to front-end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { DietComponent } from './diet/diet.component';
+import { WorkoutComponent } from './workout/workout.component';
+import { PerformanceComponent } from './performance/performance.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard/diet', component: DietComponent},
+  {path: 'dashboard/workout', component: WorkoutComponent},
+  {path: 'dashboard/performance', component: PerformanceComponent},
+  {path: 'dashboard/profile', component: ProfileComponent},
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: "**", component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PerformanceComponent } from './performance/performance.component';
+import { DietComponent } from './diet/diet.component';
+import { WorkoutComponent } from './workout/workout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    RegisterComponent,
+    ProfileComponent,
+    PerformanceComponent,
+    DietComponent,
+    WorkoutComponent
   ],
   imports: [
     BrowserModule,
